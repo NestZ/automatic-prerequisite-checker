688282856b28e2a27cb6f1c5fcd17b0b08535784
NestZ <NestZ671@gmail.com>
2022-01-09 23:43:44 +0700

    add script to count erorrs from parsing

---
+++ grammar/count_err.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+source ~/.zshrc
+antlr4 -Dlanguage=Java RegCondition.g4
+javac *.java
+FAIL=0
+for filename in ../conditions/*.txt;
+do
+	OUTPUT=$(grun RegCondition s < $filename 2>&1)
+	if [[ -n "$OUTPUT" ]]
+	then
+		FAIL=$((FAIL+1))
+	fi
+	echo "$filename"
+done
+echo "$FAIL"
