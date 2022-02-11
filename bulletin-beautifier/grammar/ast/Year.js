60850b7a2f09aab0066144754493c152c37a393d
NestZ <NestZ671@gmail.com>
2022-02-11 13:15:11 +0700

    fix at least print function

--- bulletin-beautifier/grammar/ast/Year.js
+++ bulletin-beautifier/grammar/ast/Year.js
@@ -1,12 +1,12 @@
-import Atomic from "./Atomic.js";
+import Atomic from './Atomic.js';
 
 export class YearEnum {
-	static FIRST = new YearEnum("first")
-	static SECOND = new YearEnum("second")
-	static THIRD = new YearEnum("third")
-	static FOURTH = new YearEnum("fourth")
-	static FIFTH = new YearEnum("fifth")
-	static SIXTH = new YearEnum("sixth")
+	static FIRST = new YearEnum('first')
+	static SECOND = new YearEnum('second')
+	static THIRD = new YearEnum('third')
+	static FOURTH = new YearEnum('fourth')
+	static FIFTH = new YearEnum('fifth')
+	static SIXTH = new YearEnum('sixth')
   
 	constructor(name) {
 	  this.name = name
@@ -29,11 +29,12 @@ export class Year extends Atomic {
 	}
 
 	print() {
-		process.stdout.write(this.#year + " year standing");
+		if(this.#isAtLeast) process.stdout.write('at least ');
+		process.stdout.write(this.#year + ' year standing');
 	}
 
 	eval(std, courses) {
-		const stdYear = parseInt(std["year"]);
+		const stdYear = parseInt(std['year']);
 		const reqYear = parseInt(this.#year);
 		if(this.#isAtLeast) return stdYear <= reqYear;
 		else return stdYear === reqYear;
