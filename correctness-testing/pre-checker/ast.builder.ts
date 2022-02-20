77ffb6ca4a4f3f2b13cdfbb5dffab7b3050e1bbc
NestZ <NestZ671@gmail.com>
2022-02-21 00:45:00 +0700

    add data type

--- correctness-testing/pre-checker/ast.builder.ts
+++ correctness-testing/pre-checker/ast.builder.ts
@@ -1,87 +1,102 @@
 import AntlrToCondition from './antlr.to.condition';
-import Expression from './obj/Expression';
+import Condition from './obj/Condition';
 import { CharStreams, CodePointCharStream, CommonTokenStream } from 'antlr4ts';
 import { RegConditionLexer } from './parser/RegConditionLexer';
 import { ConditionContext, RegConditionParser } from './parser/RegConditionParser';
 import { load } from 'csv-load-sync';
-import Condition from './obj/Condition';
-
-type Course = {
-	courseno: string,
-	title_long_en: string,
-	degree_level: string,
-	pre_en: string,
-	year_end: string,
-	open_status: string,
-};
-
-type CourseCondition = {
-	[courseNum: string]: Condition;
-};
+import { Course, CourseCondition, FacGroupData, FacultyData, MajorData, SubMajorData } from './data.type.decl';
 
 export default class PreChecker {
-	private static faculty: any[] = null;
-	private static major: any[] = null;
-	private static subMajor: any[] = null;
-	private static facGroup: any[] = null;
+	private static faculty: FacultyData[] = null;
+	private static major: MajorData[] = null;
+	private static subMajor: SubMajorData[] = null;
+	private static facGroup: FacGroupData[] = null;
+	private static scienceBased: FacultyData[] = null;
 
-	public static getFaculty(): any[] {
+	public static getFaculty(): FacultyData[] {
 		if(PreChecker.faculty === null) {
 			PreChecker.faculty = load('./faculty.csv');
 		}
 		return PreChecker.faculty;
 	}
 
-	public static getMajor(): any[] {
+	public static getMajor(): MajorData[] {
 		if(PreChecker.major === null) {
 			PreChecker.major = load('./major.csv');
 		}
 		return PreChecker.major;
 	}
 
-	public static getSubMajor(): any[] {
+	public static getSubMajor(): SubMajorData[] {
 		if(PreChecker.subMajor === null) {
 			PreChecker.subMajor = load('./sub_major.csv');
 		}
 		return PreChecker.subMajor;
 	}
 
-	public static getFacGroup(): any[] {
+	public static getFacGroup(): FacGroupData[] {
 		if(PreChecker.facGroup === null) {
 			PreChecker.facGroup = load('./faculty_group.csv');
 		}
 		return PreChecker.facGroup;
 	}
 
+	public static getScienceBased(): FacultyData[] {
+		if(PreChecker.scienceBased === null) {
+			PreChecker.scienceBased = PreChecker.getFaculty().filter(
+				(fac: FacultyData) => {
+					if(fac.isScienceBased === '1') {
+						return true;
+					}
+				}
+			);
+		}
+		return PreChecker.scienceBased;
+	}
+
 	public static facultyId(faculty: string): string {
-		const facLst: any[] = PreChecker.getFaculty();
+		const facLst: FacultyData[] = PreChecker.getFaculty();
 		for(const fac of facLst) {
-			if((fac['facName'] as string).toLowerCase() === faculty) {
-				return fac['facId'];
+			if(fac.facName.toLowerCase() === faculty) {
+				return fac.facId;
 			}
 		}
 		throw "can't find " + faculty + ' faculty';
 	}
 
 	public static majorId(facultyId: string, major: string): string {
-		const facLst: any[] = PreChecker.getMajor().filter(
-			(fac: any) => {
-				return fac['facId'] === facultyId;
+		const facLst: MajorData[] = PreChecker.getMajor().filter(
+			(fac: MajorData) => {
+				return fac.facId === facultyId;
 			}
 		);
 		for(const fac of facLst) {
-			if(fac['majorName'].toLowerCase() === major) {
-				return fac['majorId'] as string;
+			if(fac.majorName.toLowerCase() === major) {
+				return fac.majorId;
 			}
 		}
 		throw "can't find " + major + ' major';
 	}
 
+	public static subMajorId(facultyId: string, subMajor: string): string {
+		const facLst: SubMajorData[] = PreChecker.getSubMajor().filter(
+			(fac: SubMajorData) => {
+				return fac.facId === facultyId;
+			}
+		);
+		for(const fac of facLst) {
+			if(fac.subMajorName.toLowerCase() === subMajor) {
+				return fac.subMajorId;
+			}
+		}
+		throw "can't find " + subMajor + ' sub-major';
+	}
+
 	public static facGroupId(facGroup: string) {
-		const facGroupLst: any[] = PreChecker.getFacGroup();
+		const facGroupLst: FacGroupData[] = PreChecker.getFacGroup();
 		for(const fg of facGroupLst) {
-			if(fg['facGroupName'].toLowerCase() === facGroup) {
-				return fg['facGroupId'] as string;
+			if(fg.facGroupName.toLowerCase() === facGroup) {
+				return fg.facGroupId;
 			}
 		}
 		throw "can't find " + facGroup + ' faculty group';
@@ -93,7 +108,7 @@ export default class PreChecker {
 		let astLst: CourseCondition = <CourseCondition>{};
 
 		csv.forEach((course: Course, i: number) => {
-			let inputStream: CodePointCharStream = CharStreams.fromString(course['pre_en']);
+			let inputStream: CodePointCharStream = CharStreams.fromString(course.pre_en);
 			let lexer: RegConditionLexer = new RegConditionLexer(inputStream);
 			let tokenStream: CommonTokenStream = new CommonTokenStream(lexer);
 			let parser: RegConditionParser = new RegConditionParser(tokenStream);
@@ -114,7 +129,7 @@ export default class PreChecker {
 				throw '';
 			}
 
-			const courseNum = course['courseno'];
+			const courseNum = course.courseno;
 			astLst[courseNum] = condition;
 
 			const conditionStr = astLst[courseNum].print();
---
+++ correctness-testing/pre-checker/data.type.decl.ts
@@ -0,0 +1,39 @@
+import Condition from "./obj/Condition";
+
+export type Course = {
+	courseno: string,
+	title_long_en: string,
+	degree_level: string,
+	pre_en: string,
+	year_end: string,
+	open_status: string,
+};
+
+export type CourseCondition = {
+	[courseNum: string]: Condition;
+};
+
+export type FacultyData = {
+	facId: string,
+	facName: string,
+	facGroup: string,
+	isScienceBased: string,
+};
+
+export type MajorData = {
+	facId: string,
+	majorId: string,
+	majorName: string,
+};
+
+export type SubMajorData = {
+	facId: string,
+	majorId: string,
+	subMajorId: string,
+	subMajorName: string,
+};
+
+export type FacGroupData = {
+	facGroupId: string,
+	facGroupName: string,
+};
