748ae3c2c0e6b6aae3607669250730d169223aaa
NestZ <NestZ671@gmail.com>
2022-02-26 23:57:40 +0700

    remove unused import

--- auto-prerequisite-checker/src/app.controller.ts
+++ auto-prerequisite-checker/src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, OnModuleInit } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { StudentController } from './student/student.controller';
 
 @Controller()
 export class AppController {
